26165876a2cb42725e278d0b685ef995da87b59a
Helder Santos <heldersantos.c@gmail.com>
2020-02-25 00:53:13 -0400

    transicao de fade in e fade out
    Signed-off-by: Helder Santos <heldersantos.c@gmail.com>

--- src/components/shared/painel/Painel.vue
+++ src/components/shared/painel/Painel.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="card m-2" style="width: 18rem;">
-    <div class="card-body" v-on:dblclick="visivel = !visivel">
+    <div class="card-body" v-on:click="visivel = !visivel">
       <h5 class="card-title text-center">{{ titulo }}</h5>
     </div>
-    <div v-show="visivel" @dblclick="visivel = !visivel">
-      <slot></slot>
-    </div>
+    <transition name="painel-fade">
+      <div v-show="visivel" @dblclick="visivel = !visivel">
+        <slot></slot>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -24,5 +26,14 @@ export default {
 <style scoped>
 .card {
   box-shadow: 2px 2px 10px rgba(68, 68, 68, 0.1);
+}
+.painel-fade-enter,
+.painel-fade-leave-active {
+  opacity: 0;
+}
+
+.painel-fade-enter-active,
+.painel-fade-leave-active {
+  transition: opacity 0.2s;
 }
 </style>
