9a511a9e823dc80bd5785b445049cf13e733d828
Helder Santos <heldersantos.c@gmail.com>
2020-02-24 21:11:50 -0400

    life cicle hooks - usanfo o created()
    Signed-off-by: Helder Santos <heldersantos.c@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -15,19 +15,13 @@ export default {
   data() {
     return {
       titulo: "Alurapic",
-      fotos: [
-        {
-          url:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTf1mhR-IxQn6tCJTCzsWcVj-zejS8gqiM2ZgeX9Pm1IyNUR4QN",
-          titulo: "cachorro"
-        },
-        {
-          url:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSqfoWRAin8jCcqjR0NMgU5sC5fcckTGd9Os_bYf4JvP1rZX8Ob",
-          titulo: "cachorrao"
-        }
-      ]
+      fotos: []
     };
+  },
+  //funcao que executa assim que o componente é criado
+  //life cicle hooks
+  created() {
+    alert("componente criado");
   }
 };
 </script>
