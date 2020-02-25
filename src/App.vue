4146d5f1808452a1c99f46a60321c0de8d8cf855
Helder Santos <heldersantos.c@gmail.com>
2020-02-24 20:54:22 -0400

    usando o v-for
    Signed-off-by: Helder Santos <heldersantos.c@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    <h1>{{ titulo }}</h1>
-    <img :src="foto.url" :alt="foto.titulo" />
+    <ul>
+      <li v-bind:key="foto.id" v-for="foto of fotos">
+        <h1>{{ titulo }}</h1>
+        <img :src="foto.url" :alt="foto.titulo" />
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -10,10 +14,18 @@ export default {
   data() {
     return {
       titulo: "Alurapic",
-      foto: {
-        url:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTf1mhR-IxQn6tCJTCzsWcVj-zejS8gqiM2ZgeX9Pm1IyNUR4QN"
-      }
+      fotos: [
+        {
+          url:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTf1mhR-IxQn6tCJTCzsWcVj-zejS8gqiM2ZgeX9Pm1IyNUR4QN",
+          titulo: "cachorro"
+        },
+        {
+          url:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSqfoWRAin8jCcqjR0NMgU5sC5fcckTGd9Os_bYf4JvP1rZX8Ob",
+          titulo: "cachorrao"
+        }
+      ]
     };
   }
 };
