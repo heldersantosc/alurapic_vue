b31161c22c8c2d589ab7e77093920911cffd965d
Helder Santos <heldersantos@bemol.com.br>
2020-02-26 17:52:53 -0400

    ajuste de rotas
    Signed-off-by: Helder Santos <heldersantos@bemol.com.br>

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,38 @@
 <template>
   <div class="container m-5">
-    <router-view></router-view>
+    <meu-menu :rotas='routes'></meu-menu>
+    <transition name='pagina'>
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
-export default {};
+import { routes } from "./routes";
+import Menu from './components/shared/menu/Menu.vue';
+export default {
+  components: {
+    'meu-menu' : Menu
+  },
+  data() {
+    return {
+      routes
+    };
+  }
+};
 </script>
 
 <style>
-body{
+body {
   background: black;
+}
+.pagina-enter,
+.pagina-leave-active {
+  opacity: 0;
+}
+
+.pagina-enter-active,
+.pagina-active {
+  transition: opacity 0.2s;
 }
 </style>
